7fd58f3c2c7048f092bedb0e049807cb4eb0665e
dzenthai <dzenthai@proton.me>
2024-11-07 23:56:03 +0500

    Prepare for development of security-service
    - Added initial entities for `User` and `Role`
    - Created `UserRepo` repository interface in `repository` package.
    - Configured Liquibase changelogs:
      - `master.yaml` to define changelog hierarchy.
      - `changelog-1.yaml` for initial database setup.
    - Added `application.yaml` for Spring Boot configuration.
    - Added `docker-compose.yaml` for container orchestration of the service and database.

---
+++ src/main/java/com/dzenthai/security/entity/User.java
@@ -0,0 +1,32 @@
+package com.dzenthai.security.entity;
+
+import com.dzenthai.security.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private String username;
+}
---
+++ src/main/java/com/dzenthai/security/entity/enums/Role.java
@@ -0,0 +1,7 @@
+package com.dzenthai.security.entity.enums;
+
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+}
